1fa09b3d3910124c30a2eb5bbc212659b317b0f1
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-10-02 12:43:18 +0000

    Initial changes

--- src/Register.jsx
+++ src/Register.jsx
@@ -153,7 +153,7 @@ const Register = () => {
               required
             />
             <p className="Email-text">
-              If you have an <a href='' target='_blank' rel="noopener noreferrer">SCET id</a>, then register with it. Otherwise, go with Gmail.
+              If you have an <a href='https://github.com/utsavvachhani/converse2k24' target='_blank' rel="noopener noreferrer">SCET id</a>, then register with it. Otherwise, go with Gmail.
             </p>
           </div>
     
