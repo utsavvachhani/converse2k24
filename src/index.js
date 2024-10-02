2ce70632eeea997e76b74ef30ab9aeaa36904d07
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-10-02 13:08:21 +0000

    Initial changes

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* Set the basename for GitHub Pages */}
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance (optional)
 reportWebVitals();
